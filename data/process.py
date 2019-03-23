2530f43cc3177a7887390a55e68ef76e7c7aac98
Ipsedo <berriens@gmail.com>
2019-03-23 02:02:41 +0100

    set seg size

--- data/process.py
+++ data/process.py
@@ -5,6 +5,6 @@ from scipy.signal import spectrogram
 sampling_rate = 44e3
 
 
-def spectro_seg(seg):
-    return spectrogram(seg, nperseg=3, nfft=16)
+def spectro_seg(seg, n):
+    return spectrogram(seg, nperseg=n, nfft=16)
 
