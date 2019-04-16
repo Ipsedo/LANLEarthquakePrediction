87089ae0a3f1b3fd5e6ac0ae7132a3264244cc37
Ipsedo <berriens@gmail.com>
2019-04-16 22:37:13 +0200

    fix cuda out of memory + add all label option

--- main.py
+++ main.py
@@ -2,14 +2,14 @@
 
 from data.load_save_pickle import load_pickle
 from data.process import n_fft, fft_window
-from models.conv_models import ConvModel
+from models.conv_models import ConvModel_1, ConvModel_2
 from models.hybrid_model import HybridModel
+from models.recurrent_models import MyLSTM
 from metrics.mean_absolute_error import mae
 import torch.nn as nn
 import torch as th
 from math import ceil
 from tqdm import tqdm
-import sys
 import numpy as np
 import matplotlib.pyplot as plt
 from time import sleep
@@ -17,9 +17,11 @@ from time import sleep
 
 if __name__ == "__main__":
 
+    all_label = False
+
     root_pickle = "./res/pickle/"
-    root_pickle_train = root_pickle + "train_fftwindow-50_nfft-64/"
-    root_pickle_dev = root_pickle + "dev_fftwindow-50_nfft-64/"
+    root_pickle_train = root_pickle + "train_spectro_nfft-1024_noverlap-896_nperseg-1024/"
+    root_pickle_dev = root_pickle + "dev_spectro_nfft-1024_noverlap-896_nperseg-1024/"
 
     x_train, y_train = load_pickle(root_pickle_train)
     x_dev, y_dev = load_pickle(root_pickle_dev)
@@ -27,20 +29,27 @@ if __name__ == "__main__":
     print("Data loaded !")
 
     print(x_train.shape)
+    print(y_train.shape)
+
+    x_train = (x_train - x_train.mean()) / x_train.std()
+    x_dev = (x_dev - x_dev.mean()) / x_dev.std()
 
     nb_epoch = 30
 
     batch_size = 8
     nb_batch = ceil(x_train.shape[0] / batch_size)
 
-    #m = ConvModel(n_fft)
-    m = HybridModel(n_fft, batch_size)
+    n_features = x_train.shape[1]
+
+    #m = ConvModel_2(n_features)
+    m = HybridModel(n_features, batch_size, all_label)
+    #m = MyLSTM(n_features, all_label)
     mse = nn.MSELoss()
 
     m.cuda()
     mse.cuda()
 
-    optim = th.optim.Adagrad(m.parameters(), lr=1e-4)
+    optim = th.optim.Adagrad(m.parameters(), lr=3e-4)
 
     loss_values = []
     mae_values = []
@@ -55,14 +64,17 @@ if __name__ == "__main__":
         np.random.shuffle(indices)
 
         for i in tqdm(indices):
-            i_min, i_max = i * batch_size, (i + 1) * batch_size if (i + 1) * batch_size < x_train.shape[0] else x_train.shape[0]
+            i_min, i_max = i * batch_size, (i + 1) * batch_size \
+                if (i + 1) * batch_size < x_train.shape[0] \
+                else x_train.shape[0]
 
-            x_batch, y_batch = th.Tensor(x_train[i_min:i_max]).cuda(), th.Tensor(y_train[i_min:i_max]).cuda()
+            x_batch, y_batch = th.Tensor(x_train[i_min:i_max]).cuda(), \
+                               th.Tensor(y_train[i_min:i_max]).cuda()
 
             optim.zero_grad()
 
             out = m(x_batch)
-            loss_v = mse(out, y_batch)
+            loss_v = mse(out.view(-1), y_batch.view(-1))
 
             loss_v.backward()
             optim.step()
@@ -71,29 +83,36 @@ if __name__ == "__main__":
 
         sum_loss /= nb_batch
         loss_values.append(sum_loss)
-        sleep(1e-1)
+
+        sleep(0.1)
         tqdm.write("Epoch %d, loss = %f" % (e, sum_loss))
+        sleep(0.1)
 
         m.eval()
-        m.cpu()
 
         batch_size_dev = batch_size
         nb_batch_dev = ceil(x_dev.shape[0] / batch_size_dev)
 
-        y_dev_pred = th.zeros(x_dev.shape[0])
+        y_dev_pred = th.zeros(y_dev.shape[0], y_dev.shape[1]).cuda() if all_label \
+            else th.zeros(y_dev.shape[0]).cuda()
 
-        for i in tqdm(range(nb_batch_dev)):
-            i_min, i_max = i * batch_size_dev, \
-                           (i + 1) * batch_size_dev if (i + 1) * batch_size_dev < x_dev.shape[0] else x_dev.shape[0]
+        with th.no_grad():
+            for i in tqdm(range(nb_batch_dev)):
+                i_min, i_max = i * batch_size_dev, (i + 1) * batch_size_dev \
+                                if (i + 1) * batch_size_dev < x_dev.shape[0] \
+                                else x_dev.shape[0]
 
-            x_batch = th.tensor(x_dev[i_min:i_max], requires_grad=False).float()
+                x_batch = th.tensor(x_dev[i_min:i_max]).float().cuda()
 
-            y_dev_pred[i_min:i_max] = m(x_batch).view(-1)
+                if all_label:
+                    y_dev_pred[i_min:i_max, :] = m(x_batch)
+                else:
+                    y_dev_pred[i_min:i_max] = m(x_batch).view(-1)
 
-        score = mae(y_dev_pred, th.Tensor(y_dev))
+        score = mae(y_dev_pred.view(-1), th.Tensor(y_dev).view(-1).cuda())
 
         plt.plot(np.linspace(0, 20, 2000), np.linspace(0, 20, 2000), "r-")
-        plt.scatter(y_dev, y_dev_pred.detach().numpy(), c="b", s=10, marker="o")
+        plt.scatter(y_dev, y_dev_pred.cpu().detach().numpy(), c="b", s=1e-1, marker="o")
         plt.xlabel("True value")
         plt.ylabel("Predicted value")
         plt.title("Epoch %d" % (e,))
@@ -102,9 +121,9 @@ if __name__ == "__main__":
 
         mae_values.append(score)
 
-        sleep(1e-1)
+        sleep(0.1)
         tqdm.write("Mean absolute error = %f" % (score,))
-        m.cuda()
+        sleep(0.1)
 
     model_name = "hybrid model " + str(n_fft) + "-freq " + str(fft_window) + "-windows"
 
