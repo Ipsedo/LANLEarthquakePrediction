1d492c3483561f7dbee3187d5cce10e3e1536fc4
Ipsedo <berriens@gmail.com>
2019-04-16 22:36:28 +0200

    add new model

--- models/conv_models.py
+++ models/conv_models.py
@@ -1,9 +1,9 @@
 import torch.nn as nn
 
 
-class ConvModel(nn.Module):
+class ConvModel_1(nn.Module):
     def __init__(self, nb_in_channel):
-        super(ConvModel, self).__init__()
+        super(ConvModel_1, self).__init__()
 
         self.nb_in_channel = nb_in_channel
 
@@ -23,3 +23,29 @@ class ConvModel(nn.Module):
         out = self.conv_seq(x).view(-1, self.nb_in_channel * 4 * 2)
         out = self.lin_seq(out)
         return out
+
+
+class ConvModel_2(nn.Module):
+    def __init__(self, in_channel):
+        super(ConvModel_2, self).__init__()
+
+        self.__in_channel = in_channel
+
+        self.__seq_conv = nn.Sequential(
+            nn.Conv1d(in_channel, in_channel * 6, kernel_size=3),
+            nn.ReLU(),
+            nn.Conv1d(in_channel * 6, in_channel * 16, kernel_size=7),
+            nn.ReLU(),
+            nn.Conv1d(in_channel * 16, in_channel * 24, kernel_size=9, stride=3),
+            nn.BatchNorm1d(in_channel * 24),
+            nn.MaxPool1d(100, 100)
+        )
+
+        self.__seq_lin = nn.Sequential(
+            nn.Linear(in_channel * 24 * 3, 1),
+        )
+
+    def forward(self, x):
+        out_conv = self.__seq_conv(x).view(-1, self.__in_channel * 24 * 3)
+        out_lin = self.__seq_lin(out_conv)
+        return out_lin
